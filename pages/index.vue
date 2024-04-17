b39c3d01eb69111e74f2eafe8603317e737d7906
Alexandre <87145504+0verLighT@users.noreply.github.com>
2024-04-17 18:04:18 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div class="flex h-screen flex-col justify-center items-center gap-1 text-lg">
+    <h1 class="flex justify-center items-center font-bold">Qingque.me</h1>
+    <p class="flex justify-center items-center">
+      Site en cours de développement.
+    </p>
+  </div>
+</template>
