40cf75839f477659641cf924e8e07d38ecd7ea08
Alexandre <87145504+0verLighT@users.noreply.github.com>
2024-10-11 22:53:52 +0200

    ajoute des components shadcn ui pour faire l'auth

---
+++ inertia/pages/login.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Input } from '~/components/ui/input'
+import { Button } from '~/components/ui/button'
+import { ref } from 'vue'
+
+const submit = () => {
+  console.log('submit')
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <form class="flex flex-col gap-1 w-full max-w-sm" @submit.prevent="submit">
+      <Input type="email" placeholder="Email" />
+      <Input type="password" placeholder="Password" />
+      <Button type="submit">Login</Button>
+    </form>
+  </div>
+</template>
