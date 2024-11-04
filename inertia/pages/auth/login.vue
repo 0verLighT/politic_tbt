b60ce4b0cd57ee16d310a19859bbf409059d03af
0verLighT <alexandre.kriby@gmail.com>
2024-11-04 16:24:15 +0100

    feat(auth): add link between register and login

--- inertia/pages/auth/login.vue
+++ inertia/pages/auth/login.vue
@@ -2,6 +2,7 @@
 import { Input } from '~/components/ui/input'
 import { Button } from '~/components/ui/button'
 import { useForm } from "@inertiajs/vue3";
+import { Link } from '@tuyau/inertia/vue'
 
 
 const form = useForm({
@@ -12,12 +13,19 @@ const form = useForm({
 
 <template>
   <div class="flex items-center justify-center h-screen">
-    <form class="flex flex-col gap-2 w-full max-w-sm" @submit.prevent="form.post('/auth/login')">
+    <form class="flex flex-col gap-2 w-full max-w-sm" @submit.prevent="form.post('/auth/login', {
+      onSuccess: () => form.reset()
+    })">
     <h1 class="text-2xl font-bold">Login</h1>
       <div class="flex flex-col gap-1">
         <Input type="email" placeholder="Email" name="email" v-model="form.email" />
         <Input type="password" placeholder="Password" name="password" v-model="form.password"/>
       </div>
+      <Link route="auth.register">
+        <Button variant="ghost">
+          Don't have account, Register here
+        </Button>
+      </Link>
       <Button type="submit" :disabled="form.processing">Login</Button>
     </form>
   </div>
--- inertia/pages/auth/register.vue
+++ inertia/pages/auth/register.vue
@@ -2,6 +2,7 @@
 import { Input } from '~/components/ui/input'
 import { Button } from '~/components/ui/button'
 import {useForm} from "@inertiajs/vue3";
+import { Link } from '@tuyau/inertia/vue'
 
 const form = useForm({
   name: "",
@@ -12,13 +13,20 @@ const form = useForm({
 
 <template>
   <div class="flex items-center justify-center h-screen">
-    <form class="flex flex-col gap-2 w-full max-w-sm" @submit.prevent="form.post('/auth/register')">
+    <form class="flex flex-col gap-2 w-full max-w-sm" @submit.prevent="form.post('/auth/register', {
+      onSuccess: () => form.reset()
+    })">
       <h3 class="text-2xl font-bold">Register</h3>
       <div class="flex flex-col gap-1">
         <Input type="text" placeholder="Name" name="name" v-model="form.name" />
         <Input type="email" placeholder="Email" name="email" v-model="form.email"/>
         <Input type="password" placeholder="Password" name="password" v-model="form.password"/>
       </div>
+      <Link route="auth.login">
+        <Button variant="ghost" >
+          Already have account, Connect here
+        </Button>
+      </Link>
       <Button type="submit" :disabled="form.processing">Register</Button>
     </form>
   </div>
