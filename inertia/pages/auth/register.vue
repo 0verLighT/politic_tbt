e326567191646161a755fde5d2c71389d6344785
Alexandre <87145504+0verLighT@users.noreply.github.com>
2024-11-09 23:51:41 +0100

    chore : add AuthLayout

--- inertia/pages/auth/register.vue
+++ inertia/pages/auth/register.vue
@@ -2,7 +2,9 @@
 import { Input } from '~/components/ui/input'
 import { Button } from '~/components/ui/button'
 import { useForm, Head, Link } from '@inertiajs/vue3'
+import AuthLayout from '~/layout/AuthLayout.vue'
 
+defineOptions({ layout: AuthLayout })
 defineProps<{
   errors: Record<string, string[]>
 }>()
@@ -17,34 +19,32 @@ const form = useForm({
   <Head>
     <title>Register</title>
   </Head>
-  <div class="flex items-center justify-center h-screen">
-    <form
-      class="flex flex-col gap-2 w-full max-w-sm"
-      @submit.prevent="
-        form.post('/auth/register', {
-          onSuccess: () => form.reset(),
-        })
-      "
-    >
-      <h3 class="text-2xl font-bold">Register</h3>
-      <div class="flex flex-col gap-1">
-        <Input type="text" placeholder="Name" v-model="form.name" />
-        <div v-if="form.errors.name">
-          <span class="text-red-600">{{ form.errors.name }}</span>
-        </div>
-        <Input type="email" placeholder="Email" v-model="form.email" />
-        <div v-if="form.errors.email">
-          <span class="text-red-600"> {{ form.errors.email }}</span>
-        </div>
-        <Input type="password" placeholder="Password" v-model="form.password" />
-        <div v-if="form.errors.password">
-          <span class="text-red-600">{{ form.errors.password }}</span>
-        </div>
+  <form
+    class="flex flex-col gap-2 w-full max-w-sm"
+    @submit.prevent="
+      form.post('/auth/register', {
+        onSuccess: () => form.reset(),
+      })
+    "
+  >
+    <h3 class="text-2xl font-bold">Register</h3>
+    <div class="flex flex-col gap-1">
+      <Input type="text" placeholder="Name" v-model="form.name" />
+      <div v-if="form.errors.name">
+        <span class="text-red-600">{{ form.errors.name }}</span>
       </div>
-      <Link href="/auth/login">
-        <Button variant="ghost"> Already have account, Connect here </Button>
-      </Link>
-      <Button type="submit" :disabled="form.processing">Register</Button>
-    </form>
-  </div>
+      <Input type="email" placeholder="Email" v-model="form.email" />
+      <div v-if="form.errors.email">
+        <span class="text-red-600"> {{ form.errors.email }}</span>
+      </div>
+      <Input type="password" placeholder="Password" v-model="form.password" />
+      <div v-if="form.errors.password">
+        <span class="text-red-600">{{ form.errors.password }}</span>
+      </div>
+    </div>
+    <Link href="/auth/login">
+      <Button variant="ghost"> Already have account, Connect here </Button>
+    </Link>
+    <Button type="submit" :disabled="form.processing">Register</Button>
+  </form>
 </template>
